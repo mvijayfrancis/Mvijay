c27c8420d1c7d12f15a1d6de69cef2b06277d2f1
mvijayfrancis <30342090+mvijayfrancis@users.noreply.github.com>
2017-07-29 16:55:38 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    char c;
+    printf("Enter a Character: ");
+    scanf("%c", &c);
+    if(c == 'a' || c == 'e' || c =='i' || c=='o' || c=='u' || c=='A'
+          || c=='E' || c=='I' || c=='O' || c=='U'){
+        printf("%c is a Vowel\n", c);
+    } else {
+        printf("%c is a Consonant\n", c);
+    }
+    getch();
+    return 0;
+}
