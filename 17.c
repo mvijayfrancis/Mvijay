3781ec46cc0e108d3a89f75c4c0e1cead0ab20ec
mvijayfrancis <30342090+mvijayfrancis@users.noreply.github.com>
2017-08-11 19:06:18 +0530

    Update 17.c

--- 17.c
+++ 17.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main()
 {
-    int n, i;
+    int n,i;
     unsigned long long factorial = 1;
     printf("Enter an integer: ");
     scanf("%d",&n);
     if (n < 0)
-        printf("Error! Factorial of a negative number doesn't exist.");
+    printf("Error! Factorial of a negative number doesn't exist.");
     else
     {
         for(i=1; i<=n; ++i)
         {
-            factorial *= i;              // factorial = factorial*i;
+          factorial *= i;              
         }
         printf("Factorial of %d = %llu", n, factorial);
     }
