03f2a669a915b2b8a51e580a4d08ab3a862502c1
mvijayfrancis <30342090+mvijayfrancis@users.noreply.github.com>
2017-08-11 16:41:53 +0530

    Create 12.c

---
+++ 12.c
@@ -0,0 +1,17 @@
+public class Palindrome 
+{
+    public static void main(String[] args) 
+    {
+        int num = 11221, reversedInteger = 0, remainder, originalInteger;
+        originalInteger = num;
+        for( ;num != 0; num /= 10 )
+        {
+            remainder = num % 10;
+            reversedInteger = reversedInteger * 10 + remainder;
+        }
+        if (originalInteger == reversedInteger)
+            System.out.println(originalInteger + " is a palindrome.");
+        else
+            System.out.println(originalInteger + " is not a palindrome.");
+    }
+}
