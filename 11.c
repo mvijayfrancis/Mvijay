615ec5932c4ba79f13def8e193dea31121c2fc3d
mvijayfrancis <30342090+mvijayfrancis@users.noreply.github.com>
2017-08-11 18:58:44 +0530

    Update 11.c

--- 11.c
+++ 11.c
@@ -1,17 +1,14 @@
-public class Power {
-
-    public static void main(String[] args) {
-
-        int base = 3, exponent = 4;
-
+public class Power 
+{
+    public static void main(String[]args) 
+    {
+        int base = 3, exponent=4;
         long result = 1;
-
-        while (exponent != 0)
+        while (exponent!=0)
         {
             result *= base;
             --exponent;
         }
-
         System.out.println("Answer = " + result);
     }
 }
